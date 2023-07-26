1eb2a9974bec7483e085fa48d56298f2b7d5eb1a
Johan Meyer <y-core@outlook.com>
2023-07-26 21:20:15 +0200

    Update css themes

--- assets/css/tailwind.config.js
+++ assets/css/tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const plugin = require('tailwindcss/plugin')
 module.exports = {
   darkMode: 'class',
   content: [
@@ -7,11 +9,32 @@ module.exports = {
     "./content/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Quicksand', ...defaultTheme.fontFamily.sans],
+        'serif': ['Times New Roman', ...defaultTheme.fontFamily.serif],
+      },
+    },
+    fontWeight: {
+      normal: '300',
+      medium: '300',
+      semibold: '400',
+      bold: '500',
+      extrabold: '600',
+      black: '700',
+    },
   },
   plugins: [
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        'html': { fontSize: "16px" },
+        'h1': { fontFamily: theme('fontFamily.serif'), letterSpacing: theme('letterSpacing.widest')},
+        'h2': { fontFamily: theme('fontFamily.serif'), letterSpacing: theme('letterSpacing.widest') },
+        'h3': { fontFamily: theme('fontFamily.serif'), letterSpacing: theme('letterSpacing.wider') },
+      })
+    })
   ]
 }
