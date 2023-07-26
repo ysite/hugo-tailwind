cf19755d9fd525b322bcee65b81aff1f3719bbc7
Johan Meyer <y-core@outlook.com>
2023-07-26 20:05:04 +0200

    Update light-dark theme toggle

--- assets/js/theme-manager.js
+++ assets/js/theme-manager.js
@@ -1,5 +1,5 @@
 export function getThemeState() {
-  if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+  if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage))) {
     document.documentElement.classList.add('dark')
   } else {
     document.documentElement.classList.remove('dark')
@@ -10,65 +10,25 @@ export class ThemeManager {
   constructor() {
     this.themeDarkIcon = document.getElementById("theme-toggle-dark");
     this.themeLightIcon = document.getElementById("theme-toggle-light");
-    this.themeSystemIcon = document.getElementById("theme-toggle-system");
-  }
-
-  setThemeVisibility(themeParameter) {
-    switch (themeParameter) {
-      case "system":
-        this.themeSystemIcon.classList.toggle("hidden");
-        break;
-      case "light":
-        this.themeLightIcon.classList.toggle("hidden");
-        break;
-      case "dark":
-        this.themeDarkIcon.classList.toggle("hidden");
-        break;
-    }
   }
 
   changeTheme() {
-    if (!this.themeSystemIcon.classList.contains("hidden")) {
-      this.themeSystemIcon.classList.toggle("hidden");
-      this.themeLightIcon.classList.toggle("hidden");
+    this.themeLightIcon.classList.toggle("hidden");
+    this.themeDarkIcon.classList.toggle("hidden");
+    if (this.themeLightIcon.classList.contains("hidden")) {
       localStorage.setItem("color-theme", "light");
       document.documentElement.classList.remove("dark");
-    } else if (!this.themeLightIcon.classList.contains("hidden")) {
-      this.themeLightIcon.classList.toggle("hidden");
-      this.themeDarkIcon.classList.toggle("hidden");
+    } else {
       localStorage.setItem("color-theme", "dark");
       document.documentElement.classList.add("dark");
-    } else if (!this.themeDarkIcon.classList.contains("hidden")) {
-      this.themeDarkIcon.classList.toggle("hidden");
-      this.themeSystemIcon.classList.toggle("hidden");
-      localStorage.removeItem("color-theme");
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.documentElement.classList.add("dark");
-      }
-    } else {
-      /* default fallback to system preference */
-      this.themeSystemIcon.classList.remove("hidden");
-      this.themeLightIcon.classList.add("hidden");
-      this.themeDarkIcon.classList.add("hidden");
-      localStorage.removeItem("color-theme");
-      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-        document.documentElement.classList.add("dark");
-      }
     }
   }
 
   setupThemeIndicator() {
-    let themeParameter;
-
-    /* load theme variable from local storage */
     if (localStorage.getItem("color-theme") === "dark") {
-      themeParameter = "dark";
-    } else if (localStorage.getItem("color-theme") === "light") {
-      themeParameter = "light";
+      this.themeLightIcon.classList.toggle("hidden");
     } else {
-      themeParameter = "system";
+      this.themeDarkIcon.classList.toggle("hidden");
     }
-
-    this.setThemeVisibility(themeParameter);
   }
 }
